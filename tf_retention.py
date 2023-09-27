c1ce6af241882bb1f187c90c55cf29a3619009e6
verus-carver <pranshubahadur@gmail.com>
2023-09-27 23:08:44 +0530

    tf ???

--- tf_retention.py
+++ tf_retention.py
@@ -96,5 +96,6 @@ class MultiScaleRetention(Layer):
         x = tf.split(x, self.dim//self.hdim, 2)
         x = tf.concat([headi(xi) for headi, xi in zip(self.heads, x)], -1)
         Y = self.gn(x)
-        x = self.wo(W * tf.transpose(Y, perm=[0, 2, 1]))
+        x = self.wo(W * Y))
+        print(x)
         return x
